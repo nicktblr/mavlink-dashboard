3c0b9d7c7900134d0e8bbac921cddb27b0ba3472
unknown <ntabler@ualberta.ca>
2019-03-08 08:07:02 -0800

    Add realtime animations in Matplotlib

--- mavlink-dash.py
+++ mavlink-dash.py
@@ -1,18 +1,39 @@
 import json
 import urllib.request
 import time
-urlData = 'http://127.0.0.1:56781/mavlink/'
+import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
+import numpy as np
+import seaborn
 
-while True:
+urlData = 'http://127.0.0.1:56781/mavlink/'
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+t = []
+pitch = []
+plotdata = []
+
+def getData():
     webURL = urllib.request.urlopen(urlData)
     data = webURL.read()
     encoding = webURL.info().get_content_charset('utf-8')
     jsonData = json.loads(data.decode(encoding))
-    print(jsonData['ATTITUDE']['msg']['pitch'])
-    time.sleep(0.1)
+    return jsonData['ATTITUDE']['msg']['pitch']
 
+def animate(i, xs, ys):
+    # Add x and y to lists
+    xs.append(dt.datetime.now().strftime('%H:%M:%S'))
+    ys.append(getData())
 
-    
+    # Draw x and y lists
+    ax.clear()
+    ax.plot(xs, ys)
 
+    # Format plot
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.30)
 
-
+animate = anim.FuncAnimation(fig, animate, fargs=(t,pitch), interval=1000)
+plt.show()
