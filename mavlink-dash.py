9a789ffac390da8d8c4605d2f63476d317cc71c7
Nick <ntabler@ualberta.ca>
2019-03-12 16:29:34 -0600

    Improve performance with Blitting

--- mavlink-dash.py
+++ mavlink-dash.py
@@ -7,7 +7,11 @@ import plots
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 
+from collections import namedtuple
+from functools import partial
+
 urlData = 'http://127.0.0.1:56781/mavlink/'
+plt.style.use('seaborn-darkgrid')
 
 def getMavlink():
     webURL = urllib.request.urlopen(urlData)
@@ -17,28 +21,49 @@ def getMavlink():
     return dt.datetime.now().strftime('%H:%M:%S'), jsonData['ATTITUDE']['msg']['pitch']
 
 def makeData(i, time):
-    return i%time, round(random.uniform(-90,90))
+    return i%time, round(random.uniform(-90,90)), i//time
 
-def getPlot(i, xs, ys):
-    period = 60
-    
-    # FOR TESTING
-    plotNumber = i//period
-    x, y = makeData(i, period)
-    
-    if len(xs) < plotNumber+1:
-        xs.append([])
-        ys.append([])
-    xs[plotNumber].append(x)
-    ys[plotNumber].append(y)
-    plots.animate(xs, ys, fig, ax)
+def init_fig(fig, ax, artists):
+    plt.xlim(0,period+1)
+    plt.ylim(-95,95)
+    ax.set_xlabel('Time [s]')
+    ax.set_ylabel('Pitch [degrees]')
+    ax.legend()
+    return artists
 
-t = []
-pitch = []
+def update_artists(i, artists, xs, ys):
+    x, y, station = makeData(i, period)
+    if artists[station].get_xydata().size == 0:
+        xs.clear()
+        ys.clear()
+    xs.append(x)
+    ys.append(y)
+    artists[station].set_data(xs, ys)
+    return artists
 
-plt.style.use('seaborn-darkgrid')
-fig, ax = plots.setup()
-animate = anim.FuncAnimation(fig, getPlot, fargs=(t,pitch), interval=1000)
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+xs=[]
+ys=[]
+period = 10
+
+Artists = namedtuple("Artists", ("statOne", "statTwo"))
+artists = Artists(
+    plt.plot([],[], '.', animated=True, label='Station 1')[0],
+    plt.plot([],[], '.', animated=True, label='Station 2')[0]
+)
+
+init = partial(init_fig, fig=fig, ax=ax, artists=artists)
+update = partial(update_artists, artists=artists, xs=xs, ys=ys)
+
+ani = anim.FuncAnimation(
+    fig,
+    update,
+    frames=15,
+    init_func=init,
+    interval=100,
+    blit=True,
+    repeat=False
+)
 plt.show()
-
-
--- plots.py
+++ plots.py
@@ -2,22 +2,13 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 import numpy as np
 
-def setup():
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    plt.style.use('seaborn-darkgrid')
-    return fig, ax
-
-def animate(xs, ys, fig, ax):
-    # Draw x and y lists
-    ax.clear()
-    for n in range(0,len(xs)):
-        if len(xs[n]) > 10:
-            fit = np.polyfit(xs[n],ys[n],1)
-            fit_fn = np.poly1d(fit) 
-            ax.plot(xs[n], ys[n], 'o', xs[n], fit_fn(xs[n]), '--', label="Station " + str(n+1))
-        else:
-            ax.plot(xs[n], ys[n], 'o', label="Station " + str(n+1))
-    plt.xlabel('Time [s]')
-    plt.ylabel('Pitch [degrees]')
-    ax.legend()
+# def animate(xs, ys, fig, ax):
+#     # Draw x and y lists
+#     ax.clear()
+#     for n in range(0,len(xs)):
+#         if len(xs[n]) > 10:
+#             fit = np.polyfit(xs[n],ys[n],1)
+#             fit_fn = np.poly1d(fit)
+#             ax.plot(xs[n], ys[n], 'o', xs[n], fit_fn(xs[n]), '--', label="Station " + str(n+1))
+#         else:
+#             ax.plot(xs[n], ys[n], 'o', label="Station " + str(n+1))
