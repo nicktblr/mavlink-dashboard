de4ece83a045b8baf1639f4cf301ffa4e2c1a14e
Nick <ntabler@ualberta.ca>
2019-03-12 23:28:09 -0600

    Add Linear Regression

--- mavlink-dash.py
+++ mavlink-dash.py
@@ -6,6 +6,7 @@ import datetime as dt
 import plots
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
+import numpy as np
 
 from collections import namedtuple
 from functools import partial
@@ -24,7 +25,7 @@ def makeData(i, time):
     return i%time, round(random.uniform(-90,90)), i//time
 
 def init_fig(fig, ax, artists):
-    plt.xlim(0,period+1)
+    plt.xlim(-2,period+1)
     plt.ylim(-95,95)
     ax.set_xlabel('Time [s]')
     ax.set_ylabel('Pitch [degrees]')
@@ -38,7 +39,11 @@ def update_artists(i, artists, xs, ys):
         ys.clear()
     xs.append(x)
     ys.append(y)
-    artists[station].set_data(xs, ys)
+    artists[2*station].set_data(xs, ys)
+    if len(xs) > 10:
+        fit = np.polyfit(xs,ys,1)
+        fit_fn = np.poly1d(fit)
+        artists[2*station+1].set_data(xs, fit_fn(xs))
     return artists
 
 fig = plt.figure()
@@ -48,19 +53,59 @@ xs=[]
 ys=[]
 period = 10
 
-Artists = namedtuple("Artists", ("statOne", "statTwo"))
+Artists = namedtuple("Artists",
+    ("stat1",
+     "LR1",
+     "stat2",
+     "LR2",
+     "stat3",
+     "LR3",
+     "stat4",
+     "LR4",
+     "stat5",
+     "LR5",
+     "stat6",
+     "LR6",
+     "stat7",
+     "LR7",
+     "stat8",
+     "LR8",
+     "stat9",
+     "LR9",
+     "stat10",
+     "LR10"     
+     ))
 artists = Artists(
     plt.plot([],[], '.', animated=True, label='Station 1')[0],
-    plt.plot([],[], '.', animated=True, label='Station 2')[0]
+    plt.plot([],[], '--', animated=True, label='Trend 1')[0],
+    plt.plot([],[], '.', animated=True, label='Station 2')[0],
+    plt.plot([],[], '--', animated=True, label='Trend 2')[0],
+    plt.plot([],[], '.', animated=True, label='Station 3')[0],
+    plt.plot([],[], '--', animated=True, label='Trend 3')[0],
+    plt.plot([],[], '.', animated=True, label='Station 4')[0],
+    plt.plot([],[], '--', animated=True, label='Trend 4')[0],
+    plt.plot([],[], '.', animated=True, label='Station 5')[0],
+    plt.plot([],[], '--', animated=True, label='Trend 5')[0],
+    plt.plot([],[], '.', animated=True, label='Station 6')[0],
+    plt.plot([],[], '--', animated=True, label='Trend 6')[0],
+    plt.plot([],[], '.', animated=True, label='Station 7')[0],
+    plt.plot([],[], '--', animated=True, label='Trend 7')[0],
+    plt.plot([],[], '.', animated=True, label='Station 8')[0],
+    plt.plot([],[], '--', animated=True, label='Trend 8')[0],
+    plt.plot([],[], '.', animated=True, label='Station 9')[0],
+    plt.plot([],[], '--', animated=True, label='Trend 9')[0],
+    plt.plot([],[], '.', animated=True, label='Station 10')[0],
+    plt.plot([],[], '--', animated=True, label='Trend 10')[0],
 )
 
 init = partial(init_fig, fig=fig, ax=ax, artists=artists)
 update = partial(update_artists, artists=artists, xs=xs, ys=ys)
 
+start = time.time()
+
 ani = anim.FuncAnimation(
     fig,
     update,
-    frames=15,
     init_func=init,
     interval=100,
     blit=True,
